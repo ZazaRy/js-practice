4cb6d155bcb99f9fd21671ef18a1d0b43dc5933b
Valentin Rafael <rylikanalysis@gmail.com>
2025-03-13 06:12:57 +0000

    restructured bitmap with some functions / objects

--- bastion_v3.5.js
+++ bastion_v3.5.js
@@ -178,100 +178,72 @@ const FAC_PROD_IDS = {
     7: "SPELL_SCROLL",
     8: "PAPERWORK",
     9: "ADVENTURING_GEAR",
-    10: "ARCANE_STUDY_CHARM",
-    11: "OBSERVATORY_CHARM",
-    12: "RELIQUARY_CHARM",
-    13: "SANCTUM_CHARM",
-    14: "MAGIC_ITEM_ARCANA",
-    15: "MAGIC_ITEM_RELIC",
-    16: "MAGIC_ITEM_ARMAMENT",
-    17: "MAGIC_ITEM_IMPLEMENT",
-    19: "STOCK_ARMORY",
-    20: "TRADE_GOODS",
-    21: "GAMBLING_HALL",
-    23: {name: "BASTION_DEFENDERS", description: "PLACEHOLDERS"},
-    24: {name: "ADVENTURERS_GUILD_ASSIGNENT", description: "PH"},
-    25: {name: "BAKERS_GUILD_ASSIGNENT", description: "PH"},
-    26: "BREWERS_GUILD_ASSIGNENT",
-    27: "MASONS_GUILD_ASSIGNENT",
-    28: "SHIPBUILDERS_GUILD_ASSIGNENT",
-    29: "THIEVES_GUILD_ASSIGNENT",
-    30: "CREATURE",
-    31: "SPELLCASTER",
-    32: "LIEUTENANTS",
-    33: "SOLDIERS",
-    35: "PERFUME",
-    36: "CANDLES",
-    37: "FOOD_RATIONS",
-    38: "HEALER_KIT",
-    39: "HEALING_POTION",
-    40: "ANTITOXIN",
-    41: "BASIC_POISON",
-    42: "HEALING_HERBS",
-    43: "POISON",
-    44: "TALISMAN",
-    46: "HELPFUL_LORE",
-    47: "TOPICAL_LORE",
-    48: "INFORMATION_GATHERING",
-    49: "LORE",
-    50: "TRINKET_TROPHY",
-    52: "ARCANE_RESILIENCE",
-    53: "INNER_PEACE",
-    54: "ELDRITCH_DISCOVERY",
-    55: "FORTIFYING_RITES",
-    56: "THEATRICAL_EVENT",
-    57: "TRAINING",
+    10: "MAGIC_ITEM_ARCANA",
+    11: "MAGIC_ITEM_RELIC",
+    12: "MAGIC_ITEM_ARMAMENT",
+    13: "MAGIC_ITEM_IMPLEMENT",
+    14: "STOCK_ARMORY",
+    15: "TRADE_GOODS",
+    16: "ANIMALS",
+    17: "GAMBLING_HALL",
+    18: {name: "BASTION_DEFENDERS", description: "PLACEHOLDERS"},
+    19: {name: "ADVENTURERS_GUILD_ASSIGNENT", description: "PH"},
+    20: {name: "BAKERS_GUILD_ASSIGNENT", description: "PH"},
+    21: "BREWERS_GUILD_ASSIGNENT",
+    22: "MASONS_GUILD_ASSIGNENT",
+    23: "SHIPBUILDERS_GUILD_ASSIGNENT",
+    24: "THIEVES_GUILD_ASSIGNENT",
+    25: "CREATURE",
+    26: "SPELLCASTER",
+    27: "LIEUTENANTS",
+    28: "SOLDIERS",
+    29: "PERFUME",
+    30: "CANDLES",
+    31: "FOOD_RATIONS",
+    32: "HEALER_KIT",
+    33: "HEALING_POTION",
+    34: "ANTITOXIN",
+    35: "BASIC_POISON",
+    36: "HEALING_HERBS",
+    37: "POISON",
+    38: "TALISMAN",
+    39: "HELPFUL_LORE",
+    40: "TOPICAL_LORE",
+    41: "INFORMATION_GATHERING",
+    42: "LORE",
+    43: "TRINKET_TROPHY",
+    44: "ARCANE_RESILIENCE",
+    45: "INNER_PEACE",
+    46: "ELDRITCH_DISCOVERY",
+    47: "FORTIFYING_RITES",
+    48: "THEATRICAL_EVENT",
+    49: "TRAINING",
 };
 
-const PROD_HEADER_IDS = {
-    18: "CRAFT",
-    22: "TRADE",
-    34: "RECRUIT",
-    45: "HARVEST",
-    51: "RESEARCH",
-    58: "EMPOWER",
-};
+//Product Header's Mask
+const HEADER_PROD_CRAFT = 1n << 63n;
+const HEADER_PROD_TRADE = 1n << 62n;
+const HEADER_PROD_RECRUIT = 1n << 60n;
+const HEADER_PROD_HARVEST = 1n << 59n;
+const HEADER_PROD_RESEARCH = 1n << 58n;
+const HEADER_PROD_EMPOWER = 1n << 57n;
 
 
-//Table Bit Headers
-const prod_table_mask = (1n << 65n)-1n;
+//Product Subtype's Mask
+const COUNT_CRAFTABLES = 14n;
+const COUNT_TRADEABLES = 4n;
+const COUNT_RECRUITABLES = 11n;
+const COUNT_HARVESTABLES = 10n;
+const COUNT_RESEARCHABLES = 5n;
+const COUNT_EMPOWERABLES = 6n;
 
-//Mask of all Craftables
-const HEADER_PROD_CRAFT = 1n << 18n;
-const CRAFTABLES = HEADER_PROD_CRAFT-1n;
-const MASK_CRAFTABLES = HEADER_PROD_CRAFT | CRAFTABLES;
-
-//Mask of all Tradeables
-const HEADER_PROD_TRADE = 1n << 22n;
-const TRADABLES = HEADER_PROD_TRADE-1n & ~(HEADER_PROD_CRAFT-1n|HEADER_PROD_CRAFT);
-const MASK_TRADEABLES = HEADER_PROD_TRADE | TRADABLES;
-
-//Mask of all Recruitables
-const HEADER_PROD_RECRUIT = 1n << 34n;
-const RECRUITABLES = HEADER_PROD_RECRUIT-1n & ~(HEADER_PROD_TRADE-1n|HEADER_PROD_TRADE);
-const MASK_RECRUITABLES = HEADER_PROD_RECRUIT | RECRUITABLES;
-
-
-
-
-
-
-//Mask of all Harvestables
-const HEADER_PROD_HARVEST = 1n << 45n;
-const HARVESTABLES = (HEADER_PROD_HARVEST-1n) & ~(HEADER_PROD_RECRUIT-1n|HEADER_PROD_RECRUIT);
-const MASK_HARVESTABLES = HEADER_PROD_HARVEST | HARVESTABLES;
-
-//Mask of all Reserachables
-const HEADER_PROD_RESEARCH = 1n << 51n;
-const RESEARCHABLES = (HEADER_PROD_RESEARCH-1n) & ~(HEADER_PROD_HARVEST-1n|HEADER_PROD_HARVEST);
-const MASK_RESEARCHABLES = HEADER_PROD_RESEARCH | RESEARCHABLES;
-
-//Mask of all Empowerables
-const HEADER_PROD_EMPOWER = 1n << 58n;
-const EMPOWERABLES = (HEADER_PROD_EMPOWER-1n) & ~(HEADER_PROD_RESEARCH-1n|HEADER_PROD_RESEARCH);
-const MASK_EMPOWERABLES = HEADER_PROD_EMPOWER | EMPOWERABLES;
-
-
+const MASK_CRAFTABLES = HEADER_PROD_CRAFT | ((1n << COUNT_CRAFTABLES)-1n);
+const MASK_TRADEABLES = (HEADER_PROD_TRADE | ((1n << COUNT_TRADEABLES)-1n)) << COUNT_CRAFTABLES;
+const MASK_RECRUITABLES = (HEADER_PROD_RECRUIT | ((1n << COUNT_RECRUITABLES)-1n)) << (COUNT_CRAFTABLES+COUNT_TRADEABLES);
+const MASK_HARVESTABLES = (HEADER_PROD_HARVEST | ((1n << COUNT_HARVESTABLES)-1n)) << (COUNT_CRAFTABLES+COUNT_TRADEABLES+COUNT_RECRUITABLES);
+const MASK_RESEARCHABLES = HEADER_PROD_RESEARCH | ((1n << COUNT_RESEARCHABLES)-1n) << (COUNT_CRAFTABLES+COUNT_TRADEABLES+COUNT_RECRUITABLES+COUNT_HARVESTABLES);
+const MASK_EMPOWERABLES = (HEADER_PROD_EMPOWER | ((1n << COUNT_EMPOWERABLES)-1n)) << (COUNT_CRAFTABLES+COUNT_TRADEABLES+COUNT_RECRUITABLES+COUNT_HARVESTABLES+COUNT_RESEARCHABLES);
+log((1n<<39n)&MASK_RESEARCHABLES);
 
 const FAC_SIZES = {
     0: {size: 4, name: "Cramped"},
@@ -279,42 +251,124 @@ const FAC_SIZES = {
     2: {size: 32, name: "Vast"},
 };
 
-// const ORDER_IDS ={
-//     0: "CRAFT",
-//     1: "TRADE",
-//     2: "RECRUIT",
-//     3: "HARVEST",
-//     4: "RESEARCH",
-//     5: "EMPOWER",
-// };
+const ORDER_TYPES = {
+    0: "CRAFT",
+    1: "TRADE",
+    2: "RECRUIT",
+    3: "HARVEST",
+    4: "RESEARCH",
+    5: "EMPOWER",
+};
 
 const FAC_INTRINSICS = {
     name: new Uint8Array(37),
     size: new Uint8Array(37),
     hirelings: new Uint8Array(37),
+    order_type: new Uint8Array(37),
     contains_prods: new BigUint64Array(37),
     //Pulling a facility by collumn takes 4 bytes, so 60 bytes left to fill the cache line.
 };
-FAC_INTRINSICS.contains_prods[0] = FAC_INTRINSICS.contains_prods[1] = FAC_INTRINSICS.contains_prods[2] = FAC_INTRINSICS.contains_prods[3] = FAC_INTRINSICS.contains_prods[4] = FAC_INTRINSICS.contains_prods[5] = FAC_INTRINSICS.contains_prods[6] = MASK_CRAFTABLES;
-FAC_INTRINSICS.contains_prods[7] = FAC_INTRINSICS.contains_prods[8] = FAC_INTRINSICS.contains_prods[9] = FAC_INTRINSICS.contains_prods[10] =  MASK_TRADEABLES;
-FAC_INTRINSICS.contains_prods[11] = FAC_INTRINSICS.contains_prods[12] = FAC_INTRINSICS.contains_prods[13] = FAC_INTRINSICS.contains_prods[14] = FAC_INTRINSICS.contains_prods[15] = FAC_INTRINSICS.contains_prods[16] = MASK_RECRUITABLES;
-FAC_INTRINSICS.contains_prods[17] = FAC_INTRINSICS.contains_prods[18] = FAC_INTRINSICS.contains_prods[19] = FAC_INTRINSICS.contains_prods[20] = MASK_RECRUITABLES;
-FAC_INTRINSICS.contains_prods[21] = FAC_INTRINSICS.contains_prods[22] = FAC_INTRINSICS.contains_prods[23] = FAC_INTRINSICS.contains_prods[24] = FAC_INTRINSICS.contains_prods[25] = FAC_INTRINSICS.contains_prods[26] = MASK_HARVESTABLES;
-FAC_INTRINSICS.contains_prods[27] = FAC_INTRINSICS.contains_prods[28] = FAC_INTRINSICS.contains_prods[29] = FAC_INTRINSICS.contains_prods[30] = MASK_RESEARCHABLES;
-FAC_INTRINSICS.contains_prods[31] = FAC_INTRINSICS.contains_prods[32] = FAC_INTRINSICS.contains_prods[33] = FAC_INTRINSICS.contains_prods[34] = FAC_INTRINSICS.contains_prods[35] = FAC_INTRINSICS.contains_prods[36] = MASK_EMPOWERABLES;
+// CRAFT facilities
+FAC_INTRINSICS.order_type[0] = FAC_INTRINSICS.order_type[1] =
+FAC_INTRINSICS.order_type[2] = FAC_INTRINSICS.order_type[3] =
+FAC_INTRINSICS.order_type[4] = FAC_INTRINSICS.order_type[5] =
+FAC_INTRINSICS.order_type[6] = 0;
 
+// Initialize contains_prods for each facility type
+FAC_INTRINSICS.contains_prods[0] = FAC_INTRINSICS.contains_prods[1] =
+FAC_INTRINSICS.contains_prods[2] = FAC_INTRINSICS.contains_prods[3] =
+FAC_INTRINSICS.contains_prods[4] = FAC_INTRINSICS.contains_prods[5] =
+FAC_INTRINSICS.contains_prods[6] = MASK_CRAFTABLES;
+
+FAC_INTRINSICS.contains_prods[7] = FAC_INTRINSICS.contains_prods[8] =
+FAC_INTRINSICS.contains_prods[9] = FAC_INTRINSICS.contains_prods[10] = MASK_TRADEABLES;
+
+FAC_INTRINSICS.contains_prods[11] = FAC_INTRINSICS.contains_prods[12] =
+FAC_INTRINSICS.contains_prods[13] = FAC_INTRINSICS.contains_prods[14] =
+FAC_INTRINSICS.contains_prods[15] = FAC_INTRINSICS.contains_prods[16] =
+FAC_INTRINSICS.contains_prods[17] = FAC_INTRINSICS.contains_prods[18] =
+FAC_INTRINSICS.contains_prods[19] = FAC_INTRINSICS.contains_prods[20] = MASK_RECRUITABLES;
+
+FAC_INTRINSICS.contains_prods[21] = FAC_INTRINSICS.contains_prods[22] =
+FAC_INTRINSICS.contains_prods[23] = FAC_INTRINSICS.contains_prods[24] =
+FAC_INTRINSICS.contains_prods[25] = FAC_INTRINSICS.contains_prods[26] = MASK_HARVESTABLES;
+
+FAC_INTRINSICS.contains_prods[27] = FAC_INTRINSICS.contains_prods[28] =
+FAC_INTRINSICS.contains_prods[29] = FAC_INTRINSICS.contains_prods[30] = MASK_RESEARCHABLES;
+
+FAC_INTRINSICS.contains_prods[31] = FAC_INTRINSICS.contains_prods[32] =
+FAC_INTRINSICS.contains_prods[33] = FAC_INTRINSICS.contains_prods[34] =
+FAC_INTRINSICS.contains_prods[35] = FAC_INTRINSICS.contains_prods[36] = MASK_EMPOWERABLES;
+
+// Initialize size values
 FAC_INTRINSICS.size[26] = FAC_INTRINSICS.size[32] = 0;
 
-FAC_INTRINSICS.size[0] = FAC_INTRINSICS.size[1] = FAC_INTRINSICS.size[2] = FAC_INTRINSICS.size[3] = FAC_INTRINSICS.size[4] = FAC_INTRINSICS.size[5] = 1;
-FAC_INTRINSICS.size[6] = FAC_INTRINSICS.size[7] = FAC_INTRINSICS.size[8] = FAC_INTRINSICS.size[10] = FAC_INTRINSICS.size[11] = FAC_INTRINSICS.size[19] = 1;
-FAC_INTRINSICS.size[21] = FAC_INTRINSICS.size[22] = FAC_INTRINSICS.size[23] = FAC_INTRINSICS.size[24] = FAC_INTRINSICS.size[25] = FAC_INTRINSICS.size[27] = FAC_INTRINSICS.size[28] = 1;
-FAC_INTRINSICS.size[29] = FAC_INTRINSICS.size[30] = FAC_INTRINSICS.size[33] = FAC_INTRINSICS.size[34] = 1;
+FAC_INTRINSICS.size[0] = FAC_INTRINSICS.size[1] =
+FAC_INTRINSICS.size[2] = FAC_INTRINSICS.size[3] =
+FAC_INTRINSICS.size[4] = FAC_INTRINSICS.size[5] =
+FAC_INTRINSICS.size[6] = FAC_INTRINSICS.size[7] =
+FAC_INTRINSICS.size[8] = FAC_INTRINSICS.size[10] =
+FAC_INTRINSICS.size[11] = FAC_INTRINSICS.size[19] =
+FAC_INTRINSICS.size[21] = FAC_INTRINSICS.size[22] =
+FAC_INTRINSICS.size[23] = FAC_INTRINSICS.size[24] =
+FAC_INTRINSICS.size[25] = FAC_INTRINSICS.size[27] =
+FAC_INTRINSICS.size[28] = FAC_INTRINSICS.size[29] =
+FAC_INTRINSICS.size[30] = FAC_INTRINSICS.size[33] =
+FAC_INTRINSICS.size[34] = 1;
 
-FAC_INTRINSICS.size[31] = FAC_INTRINSICS.size[9] = 2;
-FAC_INTRINSICS.size[12] = FAC_INTRINSICS.size[13] = FAC_INTRINSICS.size[14] = FAC_INTRINSICS.size[15] = FAC_INTRINSICS.size[16] = FAC_INTRINSICS.size[17] = 2;
-FAC_INTRINSICS.size[18] = FAC_INTRINSICS.size[35] = FAC_INTRINSICS.size[36] = FAC_INTRINSICS.size[20] = 2;
+FAC_INTRINSICS.size[31] = FAC_INTRINSICS.size[9] =
+FAC_INTRINSICS.size[12] = FAC_INTRINSICS.size[13] =
+FAC_INTRINSICS.size[14] = FAC_INTRINSICS.size[15] =
+FAC_INTRINSICS.size[16] = FAC_INTRINSICS.size[17] =
+FAC_INTRINSICS.size[18] = FAC_INTRINSICS.size[35] =
+FAC_INTRINSICS.size[36] = FAC_INTRINSICS.size[20] = 2;
 
 
+// CRAFT facilities (order_type 0)
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.ARCANE_STUDY] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.LABORATORY] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.SACRISTY] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.SANCTUARY] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.SCRIPTORIUM] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.SMITHY] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.WORKSHOP] = 0;
+
+// TRADE facilities (order_type 1)
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.ARMORY] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.STOREHOUSE] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.GAMING_HALL] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.STABLE] = 1;
+
+// RECRUIT facilities (order_type 2)
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.BARRACK] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.ADVENTURERS_GUILD] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.BAKERS_GUILD] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.BREWERS_GUILD] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.MASONS_GUILD] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.SHIPBUILDERS_GUILD] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.THIEVES_GUILD] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.MENAGERIE] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.TELEPORTATION_CIRCLE] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.WAR_ROOM] = 2;
+
+// HARVEST facilities (order_type 3)
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.FOOD_GARDEN] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.HERB_GARDEN] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.POISON_GARDEN] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.DECORATIVE_GARDEN] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.GREENHOUSE] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.RELIQUARY] = 3;
+
+// RESEARCH facilities (order_type 4)
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.ARCHIVE] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.LIBRARY] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.PUB] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.TROPHY_ROOM] = 4;
+
+// EMPOWER facilities (order_type 5)
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.DEMIPLANE] = FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.MEDITATION_CHAMBER] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.OBSERVATORY] = FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.SANCTUM] =
+FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.THEATER] = FAC_INTRINSICS.order_type[FAC_SPECIAL_IDS.TRAINING_AREA] = 5;
 
 const MAX_PLAYERS = 8;
 //TODO: still work in progress here, haven't decided what to do with tihs
@@ -323,8 +377,8 @@ const PLAYERS_SCHEDULE_TABLES = {
 };
 
 
-let gid = 0;
 const PLAYER_ID_NAMES_TABLE = {
+    GID: 0,
     0: "",
     1: "",
     2: "",
@@ -334,17 +388,20 @@ const PLAYER_ID_NAMES_TABLE = {
     6: "",
     7: "",
 };
-let PLAYERS_NAMES_ID_TABLE = Object.entries(PLAYER_ID_NAMES_TABLE).reduce((map,[id,name]) => {
-    map[name] = id;
-    return map;
-},{});
 
-function update_name_ids_table(){
-    PLAYERS_NAMES_ID_TABLE = Object.entries(PLAYER_ID_NAMES_TABLE).reduce((map,[id,name]) => {
-        map[name] = id;
-        return map;
-    },{});
+//This works for some reason, which is ridiculous.
+//the empty string is mapped to 7 different values.
+const PLAYERS_NAMES_ID_TABLE = {
+    "": 0,
+    "": 1,
+    "": 2,
+    "": 3,
+    "": 4,
+    "": 5,
+    "": 6,
+    "": 7,
 };
+
 
 const PLAYERS_INFO_FACILITY_TABLE = {
     facilities: new BigUint64Array(MAX_PLAYERS),
@@ -385,21 +442,25 @@ function flsb(mask){
 };
 
 function addPlayer(player_name){
+    const gid = PLAYER_ID_NAMES_TABLE.GID;
     if (gid >7){
         return "Max Players Capacity (8) reached";
     };
     PLAYER_ID_NAMES_TABLE[gid] = player_name;
-    gid += 1;
-    update_name_ids_table();
+    PLAYER_ID_NAMES_TABLE.GID++;
+    PLAYERS_NAMES_ID_TABLE[player_name] = gid;
     return `Player ${player_name} registered.`
 };
 addPlayer("Storm")
 addPlayer("Cloudstrike")
 addPlayer("Octavius")
+log(PLAYER_ID_NAMES_TABLE)
+log(PLAYERS_NAMES_ID_TABLE)
+log(PLAYER_ID_NAMES_TABLE[2])
 
 function getFacilityOrderType(fac_id){
-    const msb = Number(fmsb(FAC_INTRINSICS.contains_prods[fac_id]));
-    return PROD_HEADER_IDS[msb];
+    const order_id = FAC_INTRINSICS.order_type[fac_id];
+    return order_id;
 };
 
 function add_facility(fac_id, player_id){
@@ -412,7 +473,7 @@ function remove_facility(fac_id, player_id){
 
 
 function start_order(prod_id,player_id){
-    PLAYERS_SCHEDULE_TABLES.scheduled_orderes[player_id] |= (1n << prod_id);
+    PLAYERS_SCHEDULE_TABLES.scheduled_orderes[player_id] |= (1n << BigInt(prod_id));
 };
 
 function finish_order(prod_id, player_id){
@@ -517,3 +578,9 @@ const menu_fac_list_title = MENU_FAC_LIST_VIEW.FACILITIES_LIST_VIEW.title;
 
 const menu_fac_list_lvl5 = createTable(MENU_FAC_LIST_LEVEL_5.facilities_list_level5.buttons, CMD_WRAPPERS.add);
 const menu_fac_list_lvl5_t = MENU_FAC_LIST_LEVEL_5.facilities_list_level5.title;
+
+
+log(start_order(33,0))
+log(viewPlayerActiveOrders(0))
+log(finish_order(33,0))
+log(viewPlayerActiveOrders(0))
